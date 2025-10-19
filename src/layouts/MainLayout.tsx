7141f668c62fbb3fc51c2d357f1091114b18be9b
Amirhossien Mirzaei <amirhmirzaei747@gmail.com>
2025-10-19 20:55:33 +0330

    feat(mainlayout - formatter)

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -19,11 +19,9 @@ export default function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col text-white bg-gradient-to-b from-slate-800-300 to-slate-600 max-md:!p-4">
       <Header />
-
       <main className="flex-1 container mx-auto !p-4">
         <Outlet />
       </main>
-
       <NavigationBar />
     </div>
   );
